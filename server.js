3f2ad08e992d8f021b5322ade104614abbb9cd58
Vipul Agrawal <poonam.singhal@dnextwelt.com>
2026-04-27 12:50:50 +0200

    Force HTTPS redirect in production

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Force HTTPS in production (Railway terminates SSL and sets x-forwarded-proto)
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === 'production' && req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(301, `https://${req.headers.host}${req.url}`);
+  }
+  next();
+});
+
 app.use(express.static(path.join(__dirname)));
 
 app.get('*', (req, res) => {
